9d550d8bd2a8dfbfde04916b2a20a4631b5ca38b
Chomps <samuel-weldu@outlook.de>
2025-09-06 16:59:06 +0200

    Release v1.0.0.0: Remove IServerEntryPoint to fix build issues

--- Jellyfin.Plugin.CustomRows/PluginServiceRegistrator.cs
+++ Jellyfin.Plugin.CustomRows/PluginServiceRegistrator.cs
@@ -17,35 +17,3 @@ public class PluginServiceRegistrator : IPluginServiceRegistrator
         // For now, we keep it minimal
     }
 }
-
-/// <summary>
-/// Server entry point for plugin initialization
-/// </summary>
-public class ServerEntryPoint : IServerEntryPoint
-{
-    private readonly ILogger<ServerEntryPoint> _logger;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ServerEntryPoint"/> class.
-    /// </summary>
-    /// <param name="logger">Instance of the <see cref="ILogger{ServerEntryPoint}"/> interface.</param>
-    public ServerEntryPoint(ILogger<ServerEntryPoint> logger)
-    {
-        _logger = logger;
-    }
-
-    /// <inheritdoc />
-    public void Dispose()
-    {
-        // Cleanup if needed
-    }
-
-    /// <inheritdoc />
-    public void RunAsync()
-    {
-        _logger.LogInformation("Custom Rows Plugin: Server entry point running...");
-        
-        // Initialize the plugin
-        Plugin.Instance?.Initialize();
-    }
-}
